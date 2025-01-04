b09e46607b672a3f636b96257a2e69029026913d
AntonyHaro <antonyhpro@outlook.com>
2025-01-04 13:42:51 -0300

    introdução do cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes import register_routes
 from app.config import Config
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
     app.config.from_object(Config)
 
     register_routes(app)
