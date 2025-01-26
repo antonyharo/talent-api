d628340c5b6e48378ace523cb71fe0431b9f3db7
AntonyHaro <antonyhpro@outlook.com>
2025-01-26 16:28:43 -0300

    melhorias no código e jobs-comparator funcionando

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,7 +2,7 @@ from app.routes.hello_world_routes import hello_world_bp
 from app.routes.cv_routes import cv_bp
 from app.routes.jobs_routes import jobs_bp
 from app.routes.ask_gemini_routes import ask_gemini_bp
-from app.routes.jobs_analyzer_routes import jobs_analyzer_bp
+from app.routes.jobs_comparator_routes import jobs_comparator_bp
 
 
 def register_routes(app):
@@ -10,4 +10,4 @@ def register_routes(app):
     app.register_blueprint(cv_bp, url_prefix="/cv")
     app.register_blueprint(jobs_bp, url_prefix="/jobs")
     app.register_blueprint(ask_gemini_bp, url_prefix="/gemini")
-    app.register_blueprint(jobs_analyzer_bp, url_prefix="/jobs-analyzer")
+    app.register_blueprint(jobs_comparator_bp, url_prefix="/jobs-comparator")
---
+++ test.md
@@ -0,0 +1,4 @@
+| Coluna 1      | Coluna 2      | Coluna 3      |
+|---------------|---------------|---------------|
+| Valor 1       | Valor 2       | Valor 3       |
+| Outro valor 1 | Outro valor 2 | Outro valor 3 |
